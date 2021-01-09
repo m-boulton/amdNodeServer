9b8728d7b9552cab9965ec87570eac1f75a6088f
michael <boultonmichael@gmail.com>
2021-01-09 11:01:35 -0700

    bug fix

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const app = express();
 // ROUTES
-app.get("/", (req, res) => {
-  res.send("we are home");
-});
 // PORT LISTENERS
 app.listen(3000, () => console.log("listening on port 3000"));
+app.use(express.static("public"));
