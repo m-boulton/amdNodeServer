bbb37e098a075537a260bb7ff18046cb5e8b1494
michael <boultonmichael@gmail.com>
2021-03-06 22:59:17 -0700

    created object from req.body.payload before inserting it into the schema

--- routes/amd/amdRoutes.js
+++ routes/amd/amdRoutes.js
@@ -28,18 +28,18 @@ router
   // posts made to amd nav database
 
   .post(async (req, res) => {
-    const post = new AmdNav({
+    const post = {
       navigation: req.body.payload.navigation,
       navList: req.body.payload.navList,
       version: req.body.payload.version,
-    });
+    };
     console.log(`this is the data ${post}`);
     try {
       // Checking for authorization to change the database
       if (req.body.auth === false) {
         res.json({ message: "Password Incorrect" });
       } else {
-        const savedPost = await post.save();
+        const savedPost = await new AmdNav(post).save();
         res.json(savedPost);
         console.log("posted to Amd nav on the database");
       }
