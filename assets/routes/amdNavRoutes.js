24de6af653be362e714ab0289039b393093e15b0
michael <boultonmichael@gmail.com>
2021-06-06 00:25:11 -0600

    removed models folder and put them in the db connection file

--- assets/routes/amdNavRoutes.js
+++ assets/routes/amdNavRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const AmdNavModel = require("./../database/mongodbAmd");
+const { AmdNavModel } = require("./../database/mongodbAmd");
 const {
   amdNavVersionCheck,
   amdNavVersionUpdate,
